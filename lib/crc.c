f90d2566c8a9066ec3122612d40a6b3675b9472f
secsome <yuxuanxie@whu.edu.cn>
2023-11-21 00:59:31 +0800

    Format crc32_table

--- lib/crc.c
+++ lib/crc.c
@@ -2,7 +2,7 @@
 
 static u32 crc32_table[256] = 
 {
-    0x00000000, 0x77073096, 0xEE0E612C, 0x0990951B, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
+	0x00000000, 0x77073096, 0xEE0E612C, 0x0990951B, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
 	0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91,
 	0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7,
 	0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC, 0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5,
@@ -38,11 +38,11 @@ static u32 crc32_table[256] =
 
 u32 crc32_calculate(const void* data, u32 length, u32 crc)
 {
-    const u8* buffer = (const u8*)data;
+	const u8* buffer = (const u8*)data;
 	u32 ret = ~crc;
 
 	for (u32 i = 0; i < length; ++i)
-		ret = (ret >> 8) ^ crc32_table[*buffer++ ^ (ret & 0xFF)];
-
+		ret = (ret >> 8) ^ crc32_table[*buffer++ ^ (ret & 0xFF)];	
+	
 	return ~ret;
 }
